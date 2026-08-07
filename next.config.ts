464aa92eaadc541b862ad898e61b90552043c14f
yitongjeon315 <yitongjeon315@gmail.com>
2026-08-07 19:50:53 +0900

    deploy: configure next.js static export and github pages actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ['192.168.219.104'],
   basePath: '/jeongo'
 };
